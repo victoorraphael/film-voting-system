8c7fc7a2cd29656dbd856067208e454f87b7ddc5
Victor Raphael <56162733+victoorraphael@users.noreply.github.com>
2021-09-12 21:31:16 -0300

    feat(models): define film model

--- models/film.go
+++ models/film.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Film struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	ID        primitive.ObjectID `bson:"_id",omitempty`
 	Name      string             `bson:"name"`
 	Upvotes   int64              `bson:"upvotes"`
 	Downvotes int64              `bson:"downvotes"`
